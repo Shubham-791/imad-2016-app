b97b45d4cc0ec9b1dab59754d0baf3133095b8dd
Shubham-791 <shubhamv791@gmail.com>
2016-10-13 16:20:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,15 +3,77 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined')); 
 
+
+var articleOne={
+    title: 'Article One | Shubham Verma',
+    heading: 'Article One',
+    date: 'Oct 13 2016',
+    content: `
+         <p>
+         This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         </p>
+         <p>
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.
+         </p>
+         <p>
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.
+         </p>
+         <p>
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.This is the content for my Fist Article.
+         This is the content for my Fist Article.This is the content for my Fist Article.
+         </p>`
+};
+function createTemplate(data){
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+
+var htmltemplate = `
+<html>
+<head>
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class ="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+         <h3>
+             ${heading} 
+         </h3>
+         <div>
+             ${date}    
+         </div>
+         <div>
+         ${content} 
+         </div>
+     </div>
+</body>
+</html>
+
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {  //Whenever a get is made to '/' thsi function is executed
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
 app.get("/article-one",function (req,res){
-   res.sendFile(path.join(__dirname,"ui","article-one.html")); 
+   res.send(createTemplate(artilceOne));  
 });
 
 app.get("/article-two",function (req,res){
