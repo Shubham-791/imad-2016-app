f28164dbc74199255852b9424d40acd1c2514b69
Shubham-791 <shubhamv791@gmail.com>
2016-11-12 18:23:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,24 +69,3 @@ submit.onclick = function(){
     //Capture a list of names and render it as list
 };
 
-
-
-
-
-
-/*console.log('Loaded!');
-//Change the value of the element to a new value
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-
-// Move the image 
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';  
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);  //Every 100ms applty move right function
-}*/
