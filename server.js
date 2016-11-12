09b5801611cddbc88b30e3e079af4de184a403a3
Shubham-791 <shubhamv791@gmail.com>
2016-11-12 18:39:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,8 +120,7 @@ app.get('/ui/madi.png', function (req, res) {    //"        "       "        " '
 });
 
 app.get("/:articleName",function (req,res){         //Expressway framework will convert :articleName into variable name 
-    //articleName == article-One
-    // articles == {} content object for article one
+    
    var articleName = req.params.articleName;
    res.send(createTemplate(articles[articleName]));  
 });
@@ -130,3 +129,4 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+    
